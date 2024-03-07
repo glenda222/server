dcc753116d598193c0eba56ba1677d476be4a4e5
glenda222 <155441928+glenda222@users.noreply.github.com>
2024-03-07 21:01:34 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,31 +1,34 @@
 package main
-import "github.com/gin-gonic/gin"
+//import "github.com/gin-gonic/gin"
 import (
 	"fmt"
-//	"log"
+	"log"
 	"net/http"
 
-//	"server/router"
+	"router"
 )
 
 func main() {
-	router := gin.Default();
+	/*
+ 	router := gin.Default();
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"Foo": "Bar" ,
 		})
 	})
+
 	
 	fmt.Println("Starting server on the port 8888...")
 	router.Run("localhost:8888")
-	/*
+  */
+	
 	r := router.Router()
 
 	fmt.Println("Starting server on the port 8888...")
 	log.Fatal(http.ListenAndServe("localhost:8888", r)) // change later
 
 	//log.Fatal(http.ListenAndServe(":8888", r))
-	*/
+	
 }
 
