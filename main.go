0774eda8f878f0a4fb116d9a223a78d5352646c6
abdul-asif <149484804+abdul-asif@users.noreply.github.com>
2024-03-08 20:50:52 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,45 +1,17 @@
 package main
-import "github.com/gin-gonic/gin"
+
 import (
 	"fmt"
-	//"log"
+	"log"
 	"net/http"
-	"os"
-	//"server/router"
+
+	"server/router"
 )
 
 func main() {
-	router := gin.Default();
-
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"Foo": "Bar" ,
-		})
-	})
-
-	//Azure app service sets the port in env, can be random
-	port := os.Getenv("HTTP_PLATFORM_PORT")
-
-	if port == "" {
-		port = "8888"
-	}
-	fmt.Println("Starting server on the port " + port)
-	router.Run("localhost:" + port)
-	
-	/*
 	r := router.Router()
 
-	//Azure app service sets the port in env, can be random
-	port := os.Getenv("HTTP_PLATFORM_PORT")
-
-	if port == "" {
-		port = "8888"
-	}
-	fmt.Println("Starting server on the port " + port)
-	log.Fatal(http.ListenAndServe("localhost:" + port, r)) // change later
-
+	fmt.Println("Starting server on the port 8888...")
+	log.Fatal(http.ListenAndServe("localhost:8888", r)) // change later
 	//log.Fatal(http.ListenAndServe(":8888", r))
- */
-	
 }
-
