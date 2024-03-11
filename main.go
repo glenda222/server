8ab74eb2bcd4163fabb59e21e7f8e978326ea0bd
abdul-asif <149484804+abdul-asif@users.noreply.github.com>
2024-03-11 13:27:58 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,10 +8,14 @@ import (
 	"server/router"
 )
 
-func main() {
-	r := router.Router()
+// func main() {
+// 	r := router.Router()
 
-	fmt.Println("Starting server on the port 8888...")
-	log.Fatal(http.ListenAndServe("localhost:8888", r)) // change later
-	//log.Fatal(http.ListenAndServe(":8888", r))
+// 	fmt.Println("Starting server on the port 8888...")
+// 	log.Fatal(http.ListenAndServe("localhost:8888", r)) // change later
+// 	//log.Fatal(http.ListenAndServe(":8888", r))
+// }
+func main() {
+	fmt.Println("foo")
+	fmt.Println("bar")
 }
